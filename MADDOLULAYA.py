8a895ceffcc3e4ca351d5d9e4aad6af442513760
maddolulaya <maddolulaya@gmail.com>
2025-02-26 18:22:34 +0530

    DAY 4 ASSIGMENT

---
+++ MADDOLULAYA.py
@@ -0,0 +1,22 @@
+#Abhishek Sharma is an Indian cricketer who was born on September 4,
+# 2000 in Amritsar, Punjab. He is a left-handed batsman and a left-arm orthodox spinner.
+#his father name is raj kumar sharma nad mother name is manju sharma. he played srh team in ipl
+# and his highest individual score is 134.
+# his coach name is yuvaraj sing
+
+first_name=input("enter the player first name: ")
+last_name=input("enter the player last name: ")
+profession=input("enter the profession: ")
+birth_month=int(input("enter the birth month: "))
+birth_date=int(input("enter the birth date: "))
+birth_year=int(input("enter the birth year: "))
+birth_place=input("enter the birth place: ")
+batting_hand=input("enter the batting hand: ")
+bowling_hand=input("enter the bowling hand: ")
+father_name=input("enter the father name: ")
+mother_name=input("enter the mother name: ")
+team_name=input("enter the team_name: ")
+score=int(input('enter the score: '))
+coach_name=input("enter the coach name: ")
+
+print(f"{first_name} {last_name} is an indian {profession} who was born on {birth_month} {birth_date}\n  {birth_year} in {birth_place}.he is a {batting_hand} batsman and {bowling_hand} season spinner\n..his father name is {father_name}and mother name is {mother_name}\n.he played {team_name}team in ipl and his highest individual score is {score}\n .his coach is {coach_name}")
